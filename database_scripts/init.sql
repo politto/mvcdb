b74f47ebc5e98ffb7ecb2ddb0782621dfab7491d
nattaphat <eakacit@gmail.com>
2024-09-15 09:21:46 +0700

    first commit

---
+++ database_scripts/init.sql
@@ -0,0 +1,22 @@
+-- CREATE sequence public.log_lists_seq
+--     INCREMENT BY 1
+-- 	MINVALUE 1
+-- 	MAXVALUE 9223372036854775807
+-- 	START 1
+-- 	CACHE 1
+-- 	NO CYCLE;
+-- 
+-- CREATE sequence public.users_seq
+-- 	MINVALUE 1
+-- 	MAXVALUE 9223372036854775807
+-- 	START 1
+-- 	CACHE 1
+-- 	NO CYCLE;
+
+-- Create Table public.log_lists (
+--     log_id int4 NOT NULL DEFAULT nextval('log_lists_seq'::regclass),
+--     user_id int4 NOT NULL,
+--     description text NULL,
+--     created_at timestamp NULL,
+--     PRIMARY KEY (log_id)
+-- )
